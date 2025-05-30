1475bc815d35a4a1af15044362b5a5f814938d5b
devHksingh <hksingh.dev@gmail.com>
2025-05-30 12:11:18 +0530

    add api

---
+++ src/http/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL:import.meta.env.VITE_PUBLIC_BACKEND_URL,
+    headers:{
+        'Content-Type':'application/json'
+    }
+})
+
+const getEventData= async(data:{searchPara:string})=>{
+    return api.post(`/api/v1/events/getEventDetails`,data)
+}
+
+export {
+    getEventData
+}
